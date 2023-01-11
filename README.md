4f14b64aa57bece0d5af1553d71f3e82f4951cc5
David Leoni <noreply@davidleoni.it>
2023-01-11 22:37:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+
+
+
+
+Credits:
+
+CoderDojo Trento  coderdojotrento.it
+
+David Leoni  info@davidleoni.it
+
+Graphics: 
+
+-  file:    https://www.svgrepo.com/svg/6994/file      (CC0 License)
+-  folder:  https://www.svgrepo.com/svg/22198/folder   (CC0 License)
---
+++ run.py
@@ -0,0 +1,180 @@
+
+import sys
+import os, stat, types
+from xml.sax.saxutils import escape 
+import argparse
+
+
+
+
+def walk_tree(args, ret, top, level):   
+    
+    unrooted_dir = top[len(args.root)+1:]    
+    space = '&emsp; ' * level    
+    
+    if args.root == top:
+        the_id = 'id=albero'
+        label = ''
+    else:
+        the_id = ''
+        label = f'<div class="folder-img"></div> {escape(unrooted_dir)}'
+    
+    ret.append(f'''
+               <ul {the_id}> 
+                    
+                    <li class="directory">{label}
+                        <ul>
+                ''')
+
+    
+    
+    dirs = []
+    files = []
+        
+    for name in sorted(os.listdir(top)):
+        try:
+            p = os.path.join(top, name)
+            st = os.lstat(p)
+        except os.error:
+            print('Error, skipping', p)
+            continue
+        
+        unrooted_dir = top[len(args.root)+1:]
+        space = '&emsp; ' * level
+        
+        
+        if stat.S_ISDIR(st.st_mode):
+            dirs.append(name)   
+        else:
+            #TODO better filter
+            if name.endswith('.sb3'):
+                files.append(name)
+    
+    dirs.sort()
+    files.sort()
+    
+    
+    for name in dirs:                
+        walk_tree (args, ret, os.path.join(top, name), level + 1)                        
+
+    if len(files) > 0:
+        ret.append('''
+                   <li>                        
+                   ''')
+        ret.append('<table class="fileList">\n')
+        for name in files:
+            prj_url = f'{args.server_url}{os.path.join(unrooted_dir,name)}'
+            ret.append(f'''
+                        <tr>
+                                
+                                <td class="file"><div class="file-img"></div>{escape(name)}</td>
+                                <td class="file"><a href="https://turbowarp.org/embed.html?project_url={prj_url}">Vedi animazione</a></td>
+                                <td class="file"><a href="https://turbowarp.org/editor?project_url={prj_url}">Vedi codice</a></td>
+                                <td class="file"><a href="{prj_url}">Scarica file</a></td>                
+                        </tr>''')
+        ret.append('''
+                            </table>
+                        </li>                    
+                   ''')
+    ret.append('''      
+                        </ul>
+                    </li>
+                </ul>
+                ''')
+    
+
+def make_html_dirs(args):
+    
+    ret = []
+    walk_tree(args, ret, args.root, 0)
+    return ''.join(ret)
+
+def make_html_page(top):
+    preamble = '''
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+    <html>
+        <head>
+            <title>Progetti Scratch</title>            
+            <link rel="stylesheet" href="js-lists.css">
+            <link rel="stylesheet" href="sb3-to-turbowarp.css">            
+            <script src="js-lists.js"></script>            
+        </head>
+        <body>
+        <h1>Progetti Scratch</h1>\n'''
+    
+    return '\n'.join([preamble,
+                      make_html_dirs(top),
+                      '''
+                            
+                            <script>                                    
+                                    JSLists.createTree("albero");  
+//document.getElementById("alberotmp00_tgl").classList.toggle('jsl-list-open');
+document.getElementById("alberotmp00_tgl").style.display = 'none';
+                            document.getElementById("alberotmp00_tgl").parentElement.lastElementChild.classList.toggle('jsl-open');                                    
+                            </script>                      
+                        </body>
+                      </html>
+                      '''])
+                   
+if __name__ == '__main__':
+    
+
+    parser = argparse.ArgumentParser(
+                    prog = 'Sb3 to TurboWarp',
+                    description = 'What the program does',
+                    epilog = 'Text at the bottom of help')
+
+    parser.add_argument('-d', '--debug', 
+                        dest='debug',
+                        action='store_true')  # on/off flag
+    
+    parser.add_argument('-r', '--root', 
+                        dest='root',
+                        default='projects') 
+    
+    parser.add_argument('-s', '--server', 
+                        dest='server_url',
+                        default='')  
+        
+    
+    args = parser.parse_args()    
+    
+    
+    args.root = 'projects'
+    res = make_html_page(args)
+    
+    with open(f'{args.root}/index.html', 'w', encoding='utf8') as write_f:
+
+        if not args.debug:
+            print('packing...')
+
+            from bs4 import BeautifulSoup
+
+
+            soup = BeautifulSoup(res, "lxml")    
+            links = list(soup.select("link"))
+            for tag in links:
+                print('Reading link', tag['href'])
+                with open(tag['href'], 'r', encoding='utf8') as link_f:
+                    t = link_f.read()
+                    #print(t)
+                    new_tag = soup.new_tag("style")
+                    new_tag.string = t
+                    tag.replace_with(new_tag)
+            
+            scripts = list(soup.select('script["src"]'))
+            for tag in scripts:
+                print('Reading script', tag['src'])
+                with open(tag['src'], 'r', encoding='utf8') as link_f:
+                    t = link_f.read()
+                    #print(t)
+                    new_tag = soup.new_tag("script")                    
+                    new_tag.string = t
+                    tag.replace_with(new_tag)
+                
+            processed_res = soup.prettify('utf8').decode()
+            
+        else:
+            processed_res = res
+         
+        write_f.write(processed_res)
